51aaa475ced4544f0378c8b87c1a34b5fac188f6
tigres_600 <tigres_600@msn.com>
2024-04-25 03:59:27 +0000

    added links in data

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
+import { links } from '@/lib/data'
+import Link from 'next/link'
 
 export default function Header() {
   return (
@@ -10,10 +12,22 @@ export default function Header() {
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-      >
-        
-      </motion.div>
-            
+      ></motion.div>
+
+             {/* this is nav bar links, they get the hash and names from the data.ts file */}
+            <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+              <ul className='flex'>
+                {
+                links.map(link => (
+                  <li key={link.hash}>
+                    <Link href={link.hash}>
+                      {link.name}
+                      </Link>
+                  </li>
+                ))
+                }
+              </ul>
+            </nav>
     </header>
   )
 }
---
+++ lib/data.ts
@@ -0,0 +1,32 @@
+import React from "react";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
