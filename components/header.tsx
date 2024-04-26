a53e1b51090b78ffc255b288c35eb96b0af47355
tigres_600 <tigres_600@msn.com>
2024-04-26 12:29:32 +0000

    added flex to ul and Link

--- components/header.tsx
+++ components/header.tsx
@@ -16,14 +16,14 @@ export default function Header() {
 
              {/* this is nav bar links, they get the hash and names from the data.ts file */}
             <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-              <ul className='flex'>
+              <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w[initial] sm:flex-nowrap sm:gap-5'>
                 {
                 links.map(link => (
-                  <li key={link.hash}>
-                    <Link href={link.hash}>
+                  <motion.li className='h-3/4 flex items-center justify-center' key={link.hash}>
+                    <Link className='flex w-full items-center justify-center px-3 py-3 hover: text-gray-950 transition' href={link.hash}>
                       {link.name}
                       </Link>
-                  </li>
+                  </motion.li>
                 ))
                 }
               </ul>
