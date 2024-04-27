ff88648f4af8f8e4d38d50fbf9492617b6c78ee0
tigres_600 <tigres_600@msn.com>
2024-04-27 21:04:17 +0000

    picture added to intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
 
 
 export default function Intro() {
   return (
     <section>
-      
+      <div className='flex items-center justify-center'>
+        <div>
+          <Image
+            src='/public/gus-profile-pic.jpg'
+            alt='Gus Portrait'
+            width='192'
+            height='192'
+            quality='95'
+            priority={true}
+            className='h-24 w-24 rounded-full object-cover border-[0.35] border-white shadow-xl'
+          />
+
+          <span>
+            👋
+            </span>
+        </div>
+      </div>
     </section>
   )
 }
