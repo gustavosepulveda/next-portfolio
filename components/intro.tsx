89e30107851bf1bdb401baab2ba1eebdbce8625a
tigres_600 <tigres_600@msn.com>
2024-04-30 00:47:24 +0000

    added text intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -46,6 +46,13 @@ export default function Intro() {
             </motion.span>
         </div>
       </div>
+      <p>
+      <span className="font-bold">Hello, I'm Gustavo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </p>
     </section>
   )
 }
