6be09a022aa9bce3e19bca783bf562f2675e0dd2
tigres_600 <gustavosepulveda@ymail.com>
2024-05-03 02:56:34 +0000

    added hover on intro links

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,22 +59,42 @@ export default function Intro() {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
-      <div>
-        <Link href="#contact" className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full'>
-          Contact me here<BsArrowRight  />{""}
+
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium' 
+      initial={{opacity: 0, y: 100}}
+      animate={{opacity: 1, y: 0}}
+      transition={{
+        delay: 0.1
+      }}
+      >
+        <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+          
+          Contact me here
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'  />{""}
           
         </Link>
-        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
-          Download Resume<HiDownload />
+
+        <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
+          active:scale-105 transition cursor-pointer borderBlack ' href='/CV.pdf' download>
+          Download Resume
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
         </a>
-        <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full'>
+
+        <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+                 href="https://www.linkedin.com/in/gustavo-sepulveda-56a9b880/"
+                 target="_blank"
+                 >
           <BsLinkedin />
         </a>
-        <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full'>
+
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+                 href="https://github.com/gustavosepulveda"
+                 target="_blank"
+                 >
           <FaGithubSquare />
         </a>
         
-      </div>
+      </motion.div>
     </section>
   )
 }
