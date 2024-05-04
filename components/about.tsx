0fbf6cc2d43ddd9f6ffec97c1ab22b8485605334
tigres_600 <gustavosepulveda@ymail.com>
2024-05-04 22:50:20 +0000

    added About Me

---
+++ components/about.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+        <h2 className='text-3xl font-medium capitalize mb-8'>
+            About Me
+        </h2>
+
+
+        <p className='mb-3'>
+          After working in the IT field for several years , I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned{" "}
+          <span className="font-medium">full-stack web development</span>.{" "}
+          <span className="italic">My favorite part of programming</span> is the
+          problem-solving aspect. I <span className="underline">love</span> the
+          feeling of finally figuring out a solution to a problem. My core stack
+          is{" "}
+          <span className="font-medium">
+            React, Next.js, Node.js, and MongoDB
+          </span>
+          . I am also familiar with TypeScript. I am always looking to
+          learn new technologies. I am currently looking for a{" "}
+          <span className="font-medium">full-time position</span> as a software
+          developer.
+        </p>
+
+        <p>
+          <span className="italic">When I'm not coding</span>, I enjoy playing
+          video games, watching movies, and taking trips .
+        </p>
+    </section>
+  )
+}
