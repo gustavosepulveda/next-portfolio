2271fd734beb28a05bf1dc24289b3ba0bf0394b2
tigres_600 <gustavosepulveda@ymail.com>
2024-05-06 20:28:36 +0000

    added section-heading

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import SectionHeading from './section-heading'
 
 export default function About() {
   return (
     <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
-        <h2 className='text-3xl font-medium capitalize mb-8'>
-            About Me
-        </h2>
+      <SectionHeading>About Me</SectionHeading>
 
 
         <p className='mb-3'>
