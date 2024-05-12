46b714e448848bc12078c1e9290ad91693ecde15
tigres_600 <gustavosepulveda@ymail.com>
2024-05-12 00:44:10 +0000

    started projects file

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import React from 'react'
 import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
 
 export default function About() {
   return (
-    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40' 
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{delay: 0.175}}
+    >
       <SectionHeading>About Me</SectionHeading>
 
 
@@ -27,6 +34,6 @@ export default function About() {
           <span className="italic">When I'm not coding</span>, I enjoy playing
           video games, watching movies, and taking trips .
         </p>
-    </section>
+    </motion.section>
   )
 }
---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Projects() {
+  return (
+    <section>
+        <SectionHeading>My projects</SectionHeading>
+    </section>
+  )
+}
