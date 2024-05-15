26c1bbbf7cc533bfb7a9d9ab879f4e54d50aa4dd
tigres_600 <gustavosepulveda@ymail.com>
2024-05-15 23:18:19 +0000

    added styling to projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -23,12 +23,12 @@ type ProjectProps = typeof projectsData[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className='bg-gray-100 max-w-[42rem] border border-black\5 overflow-hidden sm:pr-8 relative sm:h-[20rem]'>
+    <section className='group bg-gray-100 max-w-[42rem] border border-black\5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8'>
 
       <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
         <h3 className='text-2xl font-semibold'>{title}</h3>
         <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
-        <ul className='flex flex-wrap gap-2 mt-4' >  
+        <ul className='flex flex-wrap gap-2 mt-4 sm:mt-auto' >  
           {tags.map((tag, index) => (
             <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
           ))}
@@ -37,7 +37,7 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
       </div>
 
 
-    <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl' />
+    <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-0 group-even:-left-40' />
   </section>
   )
 }
