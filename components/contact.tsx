7ae4d472375ada8c14c385cd283220dc6369c0cf
tigres_600 <gustavosepulveda@ymail.com>
2024-07-19 03:00:45 +0000

    added to contact to page.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import SectionHeading from '@/components/section-heading'
+import { FaPaperPlane } from 'react-icons/fa'
 
 export default function Contact() {
   return (
-    <section id='contact'></section>
+    <section id='contact' className="mb-20 sm:mb-28 w-[min(100%, 38rem)]">
+      <SectionHeading>Contact Me</SectionHeading>
+      <p>Please contact me directly at <a className="underline" href="mailto:gustavosepulveda@ymail.com">gustavosepulveda@ymail.com</a></p>
+      <form>
+        <input type="email" />
+        <textarea />
+        <button type="submit">
+          Submit <FaPaperPlane />
+          </button>
+      </form>
+
+    </section>
   )
 }
