ee287041625d7c44212d93108004f1f7f4d0fd94
tigres_600 <gustavosepulveda@ymail.com>
2024-07-22 02:07:37 +0000

    added borderBlack

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,10 +6,10 @@ export default function Contact() {
   return (
     <section id='contact' className="mb-20 sm:mb-28 w-[min(100%, 38rem)]">
       <SectionHeading>Contact Me</SectionHeading>
-      <p>Please contact me directly at <a className="underline" href="mailto:gustavosepulveda@ymail.com">gustavosepulveda@ymail.com</a></p>
-      <form>
-        <input type="email" />
-        <textarea />
+      <p className="text-gray-700">Please contact me directly at <a className="underline" href="mailto:gustavosepulveda@ymail.com">gustavosepulveda@ymail.com</a></p>
+      <form className="mt-10">
+        <input className="h-14 rounded-lg borderBlack" type="email" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack" />
         <button type="submit">
           Submit <FaPaperPlane />
           </button>
