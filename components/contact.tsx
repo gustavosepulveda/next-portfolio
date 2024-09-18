ce8aa91825dd6c85810fce667f0c96d4aa7e5c93
tigres_600 <gustavosepulveda@ymail.com>
2024-09-18 21:38:37 +0000

    added server actions for contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,47 +1,69 @@
-"use client"
+"use client";
 
-import React from 'react'
-import SectionHeading from '@/components/section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
-import { motion } from 'framer-motion'
-import { useSectionInView } from '@/lib/hooks';
+import React from "react";
+import SectionHeading from "@/components/section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail"
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
 
+
+
   return (
-    <motion.section 
-     id='contact'
-     ref={ref}
-     className="mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center"
-     initial={{opacity: 0,}}
-     whileInView={{opacity: 1,}}
-     transition={{duration: 1,}}
-     viewport={{once: true,}}
-     >
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
       <SectionHeading>Contact Me</SectionHeading>
       <p className="text-gray-700 -mt-6">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:gustavosepulveda@ymail.com">
           gustavosepulveda@ymail.com
-          </a></p>
-          
-      <form className="mt-10 flex flex-col">
-        <input className="h-14 px-4 rounded-lg borderBlack"
-        type="email"
-        placeholder="Your email"
-        />
-        <textarea className="h-52 p-4 my-3 rounded-lg borderBlack" 
-        placeholder="Your message" 
-        />
-        <button type="submit"
-         className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950
-         active:scale-105"
-         >
-          Submit <FaPaperPlane className=" text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " /> {" "}
-          </button>
-      </form>
+        </a>
+      </p>
 
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          console.log("Running on client");
+          console.log(formData.get("senderEmail"));
+          console.log(formData.get("message"));
+
+          await sendEmail(formData);
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 p-4 my-3 rounded-lg borderBlack"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={500}
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950
+         active:scale-105"
+        >
+          Submit{" "}
+          <FaPaperPlane className=" text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />{" "}
+        </button>
+      </form>
     </motion.section>
-  )
+  );
 }
