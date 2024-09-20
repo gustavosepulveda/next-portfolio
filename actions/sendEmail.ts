e1dd5fb0e97b86ec743df9b5d55eceb265fd9d52
tigres_600 <gustavosepulveda@ymail.com>
2024-09-20 20:53:38 +0000

    send email from contact form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,23 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("Running on server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const message = formData.get("message");
+  const senderEmail = formData.get("email");
+
+  if (!message || typeof message !== "string") {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "gustavosepulveda@ymail.com",
+    subject: "Message from contact form",
+    text: message,
+  });
 };
